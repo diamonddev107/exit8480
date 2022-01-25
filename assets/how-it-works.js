d8619c306eb9f691dc3a2ff4b5bf0dceddeeb4b1
diamonddev107 <kisney.dev.11@gmail.com>
2022-01-26 03:35:24 +0800

    homepage(how it works section)

---
+++ assets/how-it-works.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    if ( $('.hiw-wrapper .medium-half').length ) {
+        $('.hiw-wrapper .medium-half').matchHeight();
+    }
+    if ( $('.hiw-wrapper .hiw-item-text').length ) {
+        console.log('kjh-new');
+        $('.hiw-wrapper .hiw-item-text').matchHeight();
+    }
+    
+})
