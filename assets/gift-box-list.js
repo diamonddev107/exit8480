fe5dc59edc309fa9431613778a357bfce71ed7dd
diamonddev107 <kisney.dev.11@gmail.com>
2022-01-27 20:48:45 +0800

    About Us page(2 sections)

--- assets/gift-box-list.js
+++ assets/gift-box-list.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-    console.log("kjh");
     if ( $('.gbl-wrapper .gbl-item .medium-half').length ) {
         $('.gbl-wrapper .gbl-item .medium-half').matchHeight();
     }
